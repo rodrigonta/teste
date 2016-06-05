1a7a576833e8a3116df87e3f6116078a54008eac
fcandi_ <fcandi@outlook.com>
2016-06-05 22:32:14 +0000

    rota hs removida, rotas movidas para import, tabelas movidas para DBA

---
+++ DBA.hs
@@ -0,0 +1,107 @@
+{-# LANGUAGE OverloadedStrings, TypeFamilies, QuasiQuotes,
+             TemplateHaskell, GADTs, FlexibleContexts,
+             MultiParamTypeClasses, DeriveDataTypeable,
+             GeneralizedNewtypeDeriving, ViewPatterns #-}
+
+module DBAuth where
+
+import Import
+import Yesod
+import Yesod.Static
+import Data.Text
+import Database.Persist.Postgresql
+    ( ConnectionPool, SqlBackend, runSqlPool, runMigration )
+
+data Pagina = Pagina{connPool :: ConnectionPool,
+                     getStatic :: Static }
+
+staticFiles "."
+
+
+-- tabelas
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Clientex json
+   nome Text
+   cpf Text
+   endereco Text
+   telefone Text
+   cidade Text
+   estado Text
+   deriving Show
+
+Servicox json
+   tipo Text
+   preco Double 
+   descricao Text
+   empresaid EmpresaxId
+   deriving Show
+
+Empresax json
+   nome Text
+   cnpj Text
+   endereco Text
+   telefone Text
+   cidade Text
+   estado Text
+   deriving Show
+   
+Servipx json
+   tipo Text
+   preco Double 
+   descricao Text
+   prestadorid PrestadorxId
+   deriving Show
+
+Prestadorx json
+   nome Text
+   cpf Text
+   endereco Text
+   telefone Text
+   cidade Text
+   estado Text
+   deriving Show
+
+|]
+
+
+
+mkYesodData "Pagina" pRoutes
+
+instance YesodPersist Pagina where
+   type YesodPersistBackend Pagina = SqlBackend
+   runDB f = do 
+       master <- getYesod
+       let pool = connPool master
+       runSqlPool f pool
+
+instance Yesod Pagina where
+    authRoute _ = Just $ HomeR
+    isAuthorized EmpresaR _ = isAdmin
+    isAuthorized ServicoR _ = isAdmin
+    isAuthorized PrestadorR _ = isAdmin
+    isAuthorized ServipR _ = isAdmin
+    isAuthorized ExcluirclienteR _ = isUser
+    isAuthorized ListarclienteR _ = isUser
+    isAuthorized ChecarempresaR _ = isUser
+    isAuthorized ChecarservicoR _ = isUser
+    isAuthorized ChecarprestadorR _ = isUser
+    isAuthorized ChecarservipR _ = isUser
+    isAuthorized _ _ = return Authorized
+
+isAdmin = do
+    mu <- lookupSession "_ID"
+    return $ case mu of
+        Nothing -> AuthenticationRequired
+        Just "admin" -> Authorized
+        Just _ -> Unauthorized "Apenas admin acessa aqui"
+
+isUser = do
+    mu <- lookupSession "_ID"
+    return $ case mu of
+        Nothing -> AuthenticationRequired 
+        Just _ -> Authorized
+
+type Form a = Html -> MForm Handler (FormResult a, Widget)
+
+instance RenderMessage Pagina FormMessage where
+    renderMessage _ _ = defaultFormMessage
